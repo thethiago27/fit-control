33761e557dcc1f2127f7a37441352d036c736c3a
thethiago27 <thethiago27@gmail.com>
2023-09-14 19:09:43 -0400

    feat: fix pipeline

--- src/app/api/workout/current/route.ts
+++ src/app/api/workout/current/route.ts
@@ -5,19 +5,28 @@ import { cookies } from 'next/headers'
 import { JwtStrategy } from '@/infra/http/auth/jwt.strategy'
 
 export async function GET() {
-  const userId = await JwtStrategy.verifyToken()
+  try {
+    const userId = await JwtStrategy.verifyToken()
 
-  const hasWorkoutInProgress =
-    await WorkoutLogRepository.checkHasCurrentWorkout(String(userId.id))
+    const hasWorkoutInProgress =
+      await WorkoutLogRepository.checkHasCurrentWorkout(String(userId.id))
 
-  if (!hasWorkoutInProgress) {
+    if (!hasWorkoutInProgress) {
+      return NextResponse.json({
+        active: false,
+      })
+    }
+
     return NextResponse.json({
-      active: false,
+      active: !!hasWorkoutInProgress,
+      workoutTypeId: hasWorkoutInProgress.id,
+    })
+  } catch (e) {
+    return new Response('', {
+      status: 401,
+      headers: {
+        'content-type': 'application/json',
+      },
     })
   }
-
-  return NextResponse.json({
-    active: !!hasWorkoutInProgress,
-    workoutTypeId: hasWorkoutInProgress.id,
-  })
 }
