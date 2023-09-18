5482af702e5d462ac0d4fe3b159fb8996e5c0b83
thethiago27 <thethiago27@gmail.com>
2023-09-18 19:34:41 -0400

    feat: add new features

--- src/app/api/exercises/route.ts
+++ src/app/api/exercises/route.ts
@@ -1,11 +1,17 @@
 import { PrismaExercisesRepository } from '@/infra/database/prisma/repositories/exercises.repository'
 
 export async function GET() {
-  const exercise = await PrismaExercisesRepository.getAll()
+  try {
+    const exercise = await PrismaExercisesRepository.getAll()
 
-  return new Response(JSON.stringify(exercise), {
-    headers: {
-      'content-type': 'application/json;charset=UTF-8',
-    },
-  })
+    return new Response(JSON.stringify(exercise), {
+      headers: {
+        'content-type': 'application/json;charset=UTF-8',
+      },
+    })
+  } catch (e) {
+    return new Response(JSON.stringify({ error: 'Erro ao obter' }), {
+      status: 500,
+    })
+  }
 }
